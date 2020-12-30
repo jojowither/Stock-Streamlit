6311a3f4e78068a6262c7219332f488c10fcd0d4
jojowither <ms0421555@gmail.com>
2020-12-30 09:48:22 +0800

    refactor

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import twstock
 from plotly.subplots import make_subplots
 from twstock import BestFourPoint, Stock
 
-from stock import Stock_info
+from stock import StockInfo
 
 
 # 利用st.cache()快取沒有改變過的data
 @st.cache()
-def get_stock_data(stock_code, month):
-    return Stock_info(stock_code, month).get_all_data()
+def get_stock_info(stock_code, month):
+    return StockInfo(stock_code, month)
 
 # ---------------------------------- sidebar --------------------------------- #
 st.sidebar.header('Parameter Setting')
@@ -30,43 +30,21 @@ sub_info = st.sidebar.selectbox('Sub information', data_colums)
 
 st.title('股票資訊圖表')
 if stock_code and month:
-    data = get_stock_data(stock_code, month)
-    stock_name = twstock.codes[stock_code].name
-    group = twstock.codes[stock_code].group
-    start_date = twstock.codes[stock_code].start
+    stock_info = get_stock_info(stock_code, month)
+    data = stock_info.get_all_data()
 
+    stock_name = stock_info.stock_name
     st.markdown(f'# **{stock_name}**')
-
-    def iserror(func, *args, **kw):
-        try:
-            stock = func(*args, **kw)
-            return stock
-        except Exception:
-            return True
-
-    err_or_not = iserror(Stock, stock_code)  
-    if err_or_not==True:
+ 
+    if stock_info.err_or_not==True:
         st.warning('Can not load the stock analysis!')
     else:
-        stock = err_or_not  
-        trade_date = stock.date[::-1]       
-        ma5_p = stock.moving_average(stock.price, 5)[::-1]       # 計算五日均價
-        ma10_p = stock.moving_average(stock.price, 10)[::-1]
-        ma20_p = stock.moving_average(stock.price, 20)[::-1]
-        ma_c = stock.moving_average(stock.capacity, 5)[::-1]     # 計算五日均量
-        ma_br = stock.ma_bias_ratio(5, 10)[::-1]                 # 計算五日、十日乖離值
-
-        bfp = BestFourPoint(stock)
-        tobuy = bfp.best_four_point_to_buy()     # 判斷是否為四大買點
-        tosell = bfp.best_four_point_to_sell()   # 判斷是否為四大賣點
-        _bfp = bfp.best_four_point()             # 綜合判斷
 
         st.success('Load stock analysis success!')
 
         st.markdown(f'## 買賣建議')
-        # st.text(f'{tobuy}, {tosell}')
-        st.markdown(f'#### {_bfp[1]}')
-        st.markdown(f'#### {"看多，建議買" if _bfp[0] else "看空，建議賣"}')
+        st.markdown(f'#### {stock_info._bfp[1]}')
+        st.markdown(f'#### {"看多，建議買" if stock_info._bfp[0] else "看空，建議賣"}')
         st.text('')
         st.text('**投資一定有風險，基金投資有賺有賠，申購前應詳閱公開說明書。')
         st.markdown('---')
@@ -74,18 +52,18 @@ if stock_code and month:
 
         st.header(f'股票資訊')
         ma_chart = go.Figure()
-        ma_chart.add_trace(go.Scatter(x=trade_date, 
-                                    y=ma5_p,
+        ma_chart.add_trace(go.Scatter(x=stock_info.trade_date, 
+                                    y=stock_info.ma5_p,
                                     marker=dict(color='#f37121'),
                                     hovertemplate='日期%{x}<br> MA5 %{y}',
                                     name='MA5'))
-        ma_chart.add_trace(go.Scatter(x=trade_date, 
-                                    y=ma10_p,
+        ma_chart.add_trace(go.Scatter(x=stock_info.trade_date, 
+                                    y=stock_info.ma10_p,
                                     marker=dict(color='#a7c5eb'),
                                     hovertemplate='日期%{x}<br> MA10 %{y}',
                                     name='MA10'))
-        ma_chart.add_trace(go.Scatter(x=trade_date, 
-                                    y=ma20_p,
+        ma_chart.add_trace(go.Scatter(x=stock_info.trade_date, 
+                                    y=stock_info.ma20_p,
                                     marker=dict(color='#b088f9'),
                                     hovertemplate='日期%{x}<br> MA20 %{y}',
                                     name='MA20'))
